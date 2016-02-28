bec021a3cb60ca247c37a8d8c7ccf723cf54f522
liuxiaoyang <214118699@qq.com>
2016-02-28 23:39:22 +0800

    async test

--- async_test.js
+++ async_test.js
@@ -5,7 +5,17 @@ var arr = [{name:'Jack', delay: 200},
            {name:'Mike', delay: 100},
            {name:'Freewind', delay: 300}];
 
-async.each(arr, function(item, callback) {
+//async.each(arr, function(item, callback) {
+//    console.log('1 :' + item.name);
+//    setTimeout(function(){
+//        console.log('2:' + item.name);
+//        callback(null, item.name);
+//    }, item.delay);
+//}, function(err) {
+//    console.log('3 : ' + err);
+//});
+
+async.eachSeries(arr, function(item, callback) {
     console.log('1 :' + item.name);
     setTimeout(function(){
         console.log('2:' + item.name);
