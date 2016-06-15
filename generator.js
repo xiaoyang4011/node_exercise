361ce95c00b9c7c9cf88d5fdb9de43cf65d6dda2
Shawn <214118699@qq.com>
2016-06-15 11:01:26 +0800

    generator test

---
+++ generator.js
@@ -0,0 +1,15 @@
+
+
+function* f() {
+	for(let i=0; true; i++) {
+		let ret = yield i
+
+		if(ret) { i = -1}
+	}
+}
+
+var f = f();
+
+console.log(f.next()); //{ value: 0, done: false }
+console.log(f.next()); //{ value: 1, done: false }
+console.log(f.next(true)); //   ?
